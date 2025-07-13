a017ccc893da2a6879a2fd396b84d4f88c5f41fd
mehboob <mehboobmehboob090@gmail.com>
2025-07-13 10:57:21 +0500

    Refactor ProjectsData.js for improved formatting and consistency

--- constants/ProjectsData.js
+++ constants/ProjectsData.js
@@ -1,11 +1,28 @@
 export const ProjectsData = [
-    {
-        projectName: "Tax Advisor",
-        liveUrl: "https://taxadvisor.live",
-        githubUrl: "https://github.com/Mehboobiqbal-dev/Tax-advisor/",
-        projectImage: {
-            imageUrl: "https://i.ibb.co/CstcVNGv/Screenshot.png"
-        },
-        techs: ["NextJS", "TailwindCSS", "Redux Toolkit", "API Integration", "Gemini API"]
-    }
+  {
+    projectName: "Tax Advisor",
+    liveUrl: "https://taxadvisor.live",
+    githubUrl: "https://github.com/Mehboobiqbal-dev/Tax-advisor/",
+    projectImage: {
+      imageUrl: "https://i.ibb.co/CstcVNGv/Screenshot.png"
+    },
+    techs: ["NextJS", "TailwindCSS", "Redux Toolkit", "API Integration", "Gemini API"]
+  },
+  {
+    projectName: "Plantin",
+    liveUrl: "https://plantin-pink.vercel.app/",
+    githubUrl: "https://github.com/Mehboobiqbal-dev/plantin-copy",
+    projectImage: {
+      imageUrl: "https://i.ibb.co/789YqfV/Screenshot-52.png"
+    },
+    techs: ["NextJS", "TailwindCSS", "MongoDB", "Redux Toolkit"]
+  },
+  {
+    projectName: "Eztrak",
+    liveUrl: "https://eztrak.vercel.app/",
+    projectImage: {
+      imageUrl: "" // Add image URL if available
+    },
+    techs: ["Micro-Frontend", ".NET", "Redux Toolkit"]
+  }
 ];
