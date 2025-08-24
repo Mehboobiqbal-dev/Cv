77188fdc882b981134a46ae06f5427de68c1681b
mehboob <mehboobmehboob090@gmail.com>
2025-08-24 13:23:59 +0500

    Update ProjectsData.js to correct project details and improve consistency

--- constants/ProjectsData.js
+++ constants/ProjectsData.js
@@ -1,13 +1,15 @@
 export const ProjectsData = [
   {
-    projectName: "Tax Advisor",
-    liveUrl: "https://taxadvisor.live",
-    githubUrl: "https://github.com/Mehboobiqbal-dev/Tax-advisor/",
+    projectName: "CertifiedSkill.org",
+    liveUrl: "https://www.certifiedskill.org/",
+    githubUrl: "https://github.com/Mehboobiqbal-dev/certifiedskill.org",
     projectImage: {
-      imageUrl: "https://i.ibb.co/CstcVNGv/Screenshot.png"
+      imageUrl: "https://i.ibb.co/ccJDGXpL/Screenshot-2025-08-24-131434.png"
     },
-    techs: ["NextJS", "TailwindCSS", "Redux Toolkit", "API Integration", "Gemini API"]
+    techs: [ "NextJS", "TailwindCSS", "MongoDB", "Redux Toolkit" ]
+
   },
+
   {
     projectName: "Plantin",
     liveUrl: "https://plantin-pink.vercel.app/",
@@ -21,8 +23,18 @@ export const ProjectsData = [
     projectName: "Eztrak",
     liveUrl: "https://eztrak.vercel.app/",
     projectImage: {
-      imageUrl: "" // Add image URL if available
+      imageUrl: "https://i.ibb.co/hRgxrFVb/Screenshot-2025-08-24-132012.png" // Add image URL if available
     },
     techs: ["Micro-Frontend", ".NET", "Redux Toolkit"]
-  }
+  },
+  {
+    projectName: "Tax Advisor",
+    liveUrl: "https://taxadvisor.live",
+    githubUrl: "https://github.com/Mehboobiqbal-dev/Tax-advisor/",
+    projectImage: {
+      imageUrl: "https://i.ibb.co/CstcVNGv/Screenshot.png"
+    },
+    techs: ["NextJS", "TailwindCSS", "Redux Toolkit", "API Integration", "Gemini API"]
+  },
+  
 ];
